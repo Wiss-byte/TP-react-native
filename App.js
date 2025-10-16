799ffdb11f5ecec69e3ae4ff11027e75c67dd542
Wiss-byte <wissalearras@gmail.com>
2025-10-16 23:25:05 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,74 @@
+
+
+
+
+
+
+
+
+
+import React from 'react'; 
+import { Image, StyleSheet, View, Text } from 'react-native';  
+export default function App() { 
+  return ( 
+     <><View style={styles.container}>
+      <Image
+        source={require('./assets/emsi.png')}
+        style={styles.logo}
+        resizeMode="contain" />
+      <Text style={styles.schoolName}>EMSI MAARIF</Text>
+    </View><View style={styles.info}>
+        <Text style={styles.label}>
+          Nom : <Text style={styles.value}>ARRAS</Text>
+        </Text>
+        <Text style={styles.label}>
+          Prénom : <Text style={styles.value}>Wissal</Text>
+        </Text>
+        <Text style={styles.label}>
+          Année universitaire : <Text style={styles.value}>2025 / 2026</Text>
+        </Text>
+        <Text style={styles.label}>
+          Filière et groupe : <Text style={styles.value}>4IIR G6</Text>
+        </Text>
+      </View></>
+    
+  );
+}
+
+ 
+const styles = StyleSheet.create({ 
+  container: { 
+    flex: 1,                 
+    justifyContent: 'center',  
+    alignItems: 'center',      
+    backgroundColor: '#f6f7fb', 
+    flexDirection: 'row',      
+justifyContent: 'space-around',
+  }, 
+
+  logo: { 
+    width: 200, 
+    height: 200, 
+  }, 
+  schoolName: { 
+fontSize: 22, 
+fontWeight: 'bold', 
+color: '#1f2937', 
+marginTop: 20,       
+letterSpacing: 1, 
+},
+ info: { 
+    alignItems: 'center', 
+    gap: 8, 
+  }, 
+  label: { 
+    fontSize: 16, 
+    color: '#6b7280', 
+    fontWeight: '500', 
+  }, 
+  value: { 
+    fontSize: 17, 
+    color: '#111827', 
+    fontWeight: 'bold', 
+  }, 
+}); 
